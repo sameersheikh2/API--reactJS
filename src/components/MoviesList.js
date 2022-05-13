d8e00db950b624dce29bcb2fed51e69eafb70116
sameersheikh2 <sameersheikh819@gmail.com>
2022-05-13 18:15:29 +0530

    getting error in fetching data

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
 
 const MovieList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
+    <ul className={classes["movies-list"]}>
+      {props.moviess.map((movie) => (
         <Movie
           key={movie.id}
           title={movie.title}
